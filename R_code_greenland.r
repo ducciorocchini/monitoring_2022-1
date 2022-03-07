4ea570e5b69c8e521029f8197a7f001cfa6df215
Paola-Merelli <92572740+Paola-Merelli@users.noreply.github.com>
2022-03-07 10:42:49 +0100

    Update R_code_greenland.r

--- R_code_greenland.r
+++ R_code_greenland.r
@@ -1,7 +1,9 @@
-# ice melt in greenland
-# proxy: LST- land surface temperature. it is a measurement of the temperature at the soil or the highest part of vegetation.
+# Ice melt in greenland
+# proxy: LST- land surface temperature. it is a measurement of the temperature at the land level: soil or the highest part of vegetation.
+# proxy is a variable that can substitute another. In this case we expect that the higher the T the higher the melt
 
-
+# Let's set wd and recall all packages
+# remember: set wd and import data every time since R is creating temporary data for raster files, data are stored in R as temporary files so it is better to reupload every time the data
 setwd("C:/lab/greenland/")
 library(raster)
 library(ggplot2)
@@ -9,41 +11,52 @@ library(RStoolbox)
 library(patchwork)
 library(viridis)
 
-# we've downloaded some files inside a folder in virtuale and we've put them inside the greenland folder we've created inside lab folder
-# let's create a list of these files. their common pattern is lst
+# we've downloaded "data greenland" file from virtuale and we've put it inside the greenland folder we've created as a subfolder of lab folder
+# let's create a list of these files. their common pattern is "lst"
 rlist <- list.files(pattern="lst")
 rlist
-# we can see they are single layers images so to import them we use lapply function to appl the raster function
+# we can see they are single layers images so to import them we use lapply function to apply the raster function to the list
 import <- lapply(rlist, raster)
 import
-# let's create a stack: take all the data together
-# TAG ca. 35
-# 16 bit images means 2^16 values
+# now with import we can see the list of our files imported as raster layers, with all their properties presented.
+# es we can see the variability of the values which is between 0 to 65535
+# 65535 means 16 bit images since 2^16=65535 values
 
+# let's create a stack: cluster all the files together and treat them as a single file
 tgr <- stack(import)
-tgr
+tgr # u can see the total n of cells and pixels, very huge
 
+#Let's plot the all stack
 cl <- colorRampPalette(c("blue", "light blue", "pink", "yellow"))(100)
 plot(tgr, col=cl)
-# all external areas has lower temperature (???)
+# the final plot will have the yellow representing the highest temperature and blue the lowest
+# very low temperature in 2000 but along years it increases:
+# in 2010 and 2015 most of all, the areas with lower T are decreasing, the blue is rarefing meaning the temperature is increasingall external areas has lower temperature
 
-# ggplot of first and finala images 2000 vs 2015
+# now ggplot of the first and final images: 2000 vs 2015
 
+# ggplot for 2000: "p1"
 p1 <- ggplot() +
 geom_raster(tgr$lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
 scale_fill_viridis(option="magma")
 ggtitle("LST in 2000")
-# black color has a lower amount of spread in space that means there are higher T
+# black color represent the temperature 
 
+# ggplot for 2015 "p2"
 p2 <- ggplot() +
 geom_raster(tgr$lst_2015, mapping = aes(x=x, y=y, fill=lst_2015)) +
 scale_fill_viridis(option="magma") +
 ggtitle("LST in 2015")
+# here the black color (low T) has a lower amount of spread in space that means there are higher T
 
+#let's plot them together:
+p1 + p2
+# here u see well that T of 2000 was quite lower with respect to the one in 2015
+# the main point is that the lower temperatures, the ones guaranteing ice are decreasing in space, sore higher t are all around the core part of greenland
 # in extreme environment u can see direct effects of climate change
 
-# now let's look at the distribution of these data
-# notes
+# now let's look at the distribution of these data with histograms
+# notes: explains how the distribution is changing
 
 # plotting frequency sistributions of data
 par(mfrow=c(1, 2))
